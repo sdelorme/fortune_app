c368b18f3ae1b3043b0229a4b75b170b4274d749
Seth Delorme <delorme.s.m@gmail.com>
2021-02-21 11:54:37 -0600

    makes random_fortune controller using jb view

--- app/controllers/api/random_fortune_controller.rb
+++ app/controllers/api/random_fortune_controller.rb
@@ -1,2 +1,8 @@
 class Api::RandomFortuneController < ApplicationController
+  def random_fortune
+    @random_fortune = ["You will win the lottery one day", "You are going to grow 6 inches this year", "You will find your true love this year"].sample
+    puts @random_fortune
+    render "random_fortune.json.jb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,19 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+
+  namespace :api do
+    get "/random_fortune" => "random_fortune#random_fortune"
+  end
+
+  namespace :api do
+    get "/lotto" => "lotto#lotto"
+  end
+
+  namespace :api do
+    get "/visits" => "visits#visits"
+  end
+
 end
+
+
