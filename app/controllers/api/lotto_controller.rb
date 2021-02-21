45545cced6062eb92f46f94ebb35cc6516c67d4e
Seth Delorme <delorme.s.m@gmail.com>
2021-02-21 13:37:48 -0600

    fixes lotto controller and random fortune controller

--- app/controllers/api/lotto_controller.rb
+++ app/controllers/api/lotto_controller.rb
@@ -1,2 +1,10 @@
 class Api::LottoController < ApplicationController
+  def lotto
+    @lotto_numbers = []
+    6.times do
+      @lotto_numbers << rand(1..60)
+    end
+    render 'lotto.json.jb'
+  end
+
 end
